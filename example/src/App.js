5115ce23345b9cedba50ddc5da6e9df7b15b651c
trdaya <14902243+trdaya@users.noreply.github.com>
2020-06-29 19:02:23 +0200

    - added proptypes

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
 import { Button } from 'uikit'
-import 'uikit/dist/index.css'
+// import 'uikit/dist/index.css' // TODO check giving error
 
 const App = () => {
   return <Button text="Create React Library Example custom button 😄" />
 }
 
 export default App
+
+
--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 const Wrapper=styled.button`
 color:${p=>p.theme.colors.darkGreen};
@@ -7,6 +8,12 @@ padding: 10px;
 border: 2px solid black;
 `
 
-const Button= ({text,color})=>(<Wrapper color={color}>{text}</Wrapper>)
+const Button= ({text})=>(<Wrapper>{text}</Wrapper>)
+Button.defaultProps={
+  text:'Click me',
+}
+Button.propTypes={
+  text:PropTypes.string,
+}
 
 export default Button
