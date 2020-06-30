6a2d3cb171dc35b4f1ec3b46dc78c1db470a81b8
trdaya <14902243+trdaya@users.noreply.github.com>
2020-06-30 10:07:01 +0200

    - eslint in prog

--- src/components/atoms/Button/index.js
+++ src/components/atoms/Button/index.js
@@ -1,24 +1,27 @@
-import React from "react";
-import styled from 'styled-components';
-import PropTypes from 'prop-types';
-import {Types} from "./constants";
-import theme from "../../../constants/themes";
-import {getRem} from "../../../constants/css";
+import React from 'react'
+import styled from 'styled-components'
+import PropTypes from 'prop-types'
+import { Types } from './constants'
+import theme from '../../../constants/themes'
+import { getRem } from '../../../constants/css'
 
-const Wrapper=styled.button`
-color:${p=>p.type ===Types.SECONDARY?theme.colors.visitedRed:theme.colors.brightGreen};
-padding: ${getRem(10)};
-border: ${getRem(2)} solid black;
+const Wrapper = styled.button`
+  color: ${(p) =>
+    p.type === Types.SECONDARY
+      ? theme.colors.visitedRed
+      : theme.colors.brightGreen};
+  padding: ${getRem(10)};
+  border: ${getRem(2)} solid black;
 `
 
-const Button= ({text,type})=>(<Wrapper type={type}>{text}</Wrapper>)
-Button.defaultProps={
-  text:'Click me',
-  type:Types.PRIMARY
+const Button = ({ text, type }) => <Wrapper type={type}>{text}</Wrapper>
+Button.defaultProps = {
+  text: 'Click me',
+  type: Types.PRIMARY
 }
-Button.propTypes={
-  text:PropTypes.string,
-  type:PropTypes.oneOf(Object.values(Types))
+Button.propTypes = {
+  text: PropTypes.string,
+  type: PropTypes.oneOf(Object.values(Types))
 }
 
 export default Button
