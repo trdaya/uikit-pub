9fc9dff57d6adfe63feb687032af72cb38f658d4
trdaya <14902243+trdaya@users.noreply.github.com>
2020-06-30 11:31:43 +0200

    Updates

---
+++ precache-manifest.cce33a604266dead0ff50222532140e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29387055076e338808ccb2dac87738a1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "81e6736488fd986582ca",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d3c8f6005ea7245a905d",
+    "url": "./static/js/2.8a66a331.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.8a66a331.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81e6736488fd986582ca",
+    "url": "./static/js/main.5792afc9.chunk.js"
+  },
+  {
+    "revision": "85ed2807ec7bb37ae3da",
+    "url": "./static/js/runtime-main.30a3c9b9.js"
+  }
+]);
